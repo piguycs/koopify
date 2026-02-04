09fbdac27a1aa895abec203d782683e1205193e0
piguycs <piguycs@pm.me>
2026-02-04 22:06:06 +0100

    ci: changing the example response to test CI (with manual CD)

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -7,13 +7,18 @@ import (
 	"github.com/labstack/echo/v5/middleware"
 )
 
+type SimpleJsonData = struct {
+	Hello string `json:"hello"`
+	Foo   string `json:"foo"`
+}
+
 func main() {
 	e := echo.New()
 
 	e.Use(middleware.RequestLogger())
 
 	e.GET("/hello-world", func(c *echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World")
+		return c.JSON(http.StatusOK, SimpleJsonData{Hello: "world", Foo: "bar"})
 	})
 
 	if err := e.Start(":8080"); err != nil {
