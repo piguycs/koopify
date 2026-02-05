ee1126c44ae201751e9d653abcd3412e504e378f
piguycs <piguycs@pm.me>
2026-02-05 10:08:12 +0100

    adding TLS support to the API

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,32 +1,62 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v5"
 	"github.com/labstack/echo/v5/middleware"
 )
 
-type SimpleJsonData = struct {
-	Hello      string `json:"hello"`
-	Foo        string `json:"foo"`
-	ApiVersion int    `json:"apiVersion"`
-}
+var CommitHash = "dev"
 
 func main() {
 	e := echo.New()
 
 	e.Use(middleware.RequestLogger())
 
-	e.GET("/hello-world", func(c *echo.Context) error {
-		return c.JSON(http.StatusOK, SimpleJsonData{
-			Hello:      "world",
-			Foo:        "bar",
-			ApiVersion: 4,
-		})
+	e.GET("/commit", func(c *echo.Context) error {
+		return c.String(http.StatusOK, CommitHash)
 	})
 
-	if err := e.Start(":8080"); err != nil {
-		e.Logger.Error("failed to start server", "error", err)
+	cert, key, err := TlsConfig()
+	if err != nil {
+		e.Logger.Error("failed to get tls config, probably TLS_CERT or TLS_KEY is unset", "error", err)
 	}
+
+	context := context.Background()
+	sc := echo.StartConfig{
+		Address: ":8080",
+	}
+
+	if cert != nil && key != nil {
+		e.Logger.Info("starting server with TLS :)")
+		if err := sc.StartTLS(context, e, cert, key); err != nil {
+			e.Logger.Error("failed to start server", "error", err)
+		}
+	} else {
+		e.Logger.Info("starting server without TLS :(")
+		if err := sc.Start(context, e); err != nil {
+			e.Logger.Error("failed to start server", "error", err)
+		}
+	}
+
+}
+
+func TlsConfig() (cert *string, key *string, err error) {
+	tlsEnabled := os.Getenv("TLS_ENABLED")
+	tlsCert := os.Getenv("TLS_CERT")
+	tlsKey := os.Getenv("TLS_KEY")
+
+	// TLS_ENABLED=1 means it is enabled, unset or 0 means it is not. Anything above 0 is true
+	if tlsEnabled == "" || tlsEnabled == "0" {
+		return nil, nil, nil
+	}
+
+	if tlsCert == "" || tlsKey == "" {
+		return nil, nil, os.ErrNotExist
+	}
+
+	return &tlsCert, &tlsKey, nil
 }
